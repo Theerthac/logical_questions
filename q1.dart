32c8075327b3fe65200b0c1b177847303f8c65a4
Theerthac <127188105+Theerthac@users.noreply.github.com>
2023-10-11 15:26:35 +0530

    first commit

---
+++ q1.dart
@@ -0,0 +1,13 @@
+// void main(List<String> args) {
+//   int num = 7;
+//   if (num % 2 == 0) {
+//     print('Even');
+//   } else {
+//     print('Odd');
+//   }
+// }
+
+void main(List<String> args) {
+  int num = 1;
+  print(num.isEven ? 'even' : 'odd');
+}
